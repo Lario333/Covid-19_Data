a593dc1398a12a92b005acc120c788107da2cc97
Diaci <76226592+Davide-Iaci@users.noreply.github.com>
2021-05-16 13:09:20 +0200

    Update DataLinearChart.java
    details update

--- src/Source/DataLinearChart.java
+++ src/Source/DataLinearChart.java
@@ -8,13 +8,16 @@
  */
 package Source;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.LineAndShapeRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
@@ -63,6 +66,19 @@ public class DataLinearChart extends JPanel {
                 PlotOrientation.VERTICAL,
                 true, true, false);
         lineChart.setBackgroundPaint(new Color(247, 246, 242));
+        
+        CategoryPlot plot = lineChart.getCategoryPlot();
+        LineAndShapeRenderer renderer = new LineAndShapeRenderer(true, false);
+        // change of color lines
+        renderer.setSeriesPaint(0, Color.red);
+        renderer.setSeriesPaint(1, Color.darkGray);
+        renderer.setSeriesPaint(2, new Color(0, 154, 220));
+        // change of line thickness
+        renderer.setSeriesStroke(0, new BasicStroke(1.5f));
+        renderer.setSeriesStroke(1, new BasicStroke(1.5f));
+        renderer.setSeriesStroke(2, new BasicStroke(1.5f));
+        
+        plot.setRenderer(renderer);
         
         return lineChart;
     }
