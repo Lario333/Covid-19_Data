6e880e02dd7709a97ae2c4756bec6402157ccc00
Lario <59116820+Lario333@users.noreply.github.com>
2021-05-17 18:47:28 +0200

    Fixed setActiveLanguage(String title) not working

--- src/Source/Languages.java
+++ src/Source/Languages.java
@@ -25,7 +25,7 @@ public class Languages {
     }
 
     public void setActiveLanguage(String title) {
-        for(int i = 0 ; i < (languages.size() - 1); i++){
+        for(int i = 0 ; i < (languages.size()); i++){
             if (title.equals(languages.get(i).getTitle())){
                 this.activeLanguage = languages.get(i);
                 break;
