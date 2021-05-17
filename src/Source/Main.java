db43ab245a5125f64efe4ae1c4c108a23fafb138
Diaci <76226592+Davide-Iaci@users.noreply.github.com>
2021-05-17 09:16:51 +0200

    Update Main.java

--- src/Source/Main.java
+++ src/Source/Main.java
@@ -15,36 +15,37 @@ import java.util.ArrayList;
 
 /**
  * Main class
- */ 
-public class Main { 
+ */
+public class Main {
 
     static ArrayList<Day> daysData = new ArrayList<Day>();
 
-    static Languages useLanguage = new Languages(Languages.ita);
-    // Uncomment this for ita ^
-    // static Languages useLanguage = new Languages(Languages.eng);
-    // Uncomment this for english
     /**
      * Main that will run the program
-     * 
+     *
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
 
-
-
-        DataUploader loader = new DataUploader(); 
+        // If internet connecction is available run the program
+        if (!DataUploader.checkConnection()) { // if internet connection is not available
+            // Creation of JDialog with an error message
+            JOptionPane.showMessageDialog(null,
+                    "Sembrano esserci dei problemi di connessione, controllare le impostazioni", "Connessione", JOptionPane.OK_OPTION);
+            System.exit(0); // stop of program
+        }
+        DataUploader loader = new DataUploader();
 
         // Download latest data from the COVID Github Repo
         loader.downloadData();
         loader.readCsvGeneralFile(); // upload Covid-Data csv file
-        useLanguage.setActiveLanguage(loader.checkLanguage());
         Day.calcDiffData();
         // Creating the frame
         Covid19_Frame frame = new Covid19_Frame();
 
         // Frame settings
-        frame.setBackground(new Color(247,246,242)); 
+        frame.setBackground(new Color(247, 246, 242));
         frame.setTitle("Covid Graphs");
         frame.setResizable(false);
         frame.setSize(1455, 850);
